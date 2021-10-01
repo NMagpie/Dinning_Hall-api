ceb458474099b37b80d17331ea9a4162f95269c7
NMagpie <sorochin.work@gmail.com>
2021-10-01 15:55:33 +0300

    Added Table class

--- src/main/java/com/example/dinninghallapi/DinningHallApiApplication.java
+++ src/main/java/com/example/dinninghallapi/DinningHallApiApplication.java
@@ -1,13 +1,30 @@
 package com.example.dinninghallapi;
 
+import Order.Order;
+import Tables.Table;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class DinningHallApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DinningHallApiApplication.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Input number of tables");
+		
+		Table[] tables = new Table[scanner.nextInt()];
+
+		scanner.close();
+
+		int waiters = tables.length-2;
+
+
+
 	}
 
 }
